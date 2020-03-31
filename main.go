db51a149368ea7b15010e7560c7763823d0fe648
season <mimi871006@gmail.com>
2020-03-31 22:34:49 +0800

    change redis package, complete time duration

--- main.go
+++ main.go
@@ -3,72 +3,59 @@ package main
 import (
 	"log"
 	"net/http"
-	"reflect"
 	"strconv"
+	"time"
 
-	"github.com/garyburd/redigo/redis"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
 
-// 每5秒來自同一IP的請求數量不超過3次
-var time_limiter = 5
-var count_limiter = 3
+// 每10秒來自同一IP的請求數量不超過3次
+var TIME_LIMITER = 10
+var COUNT_LIMITER = 3
 
 // redis: [key]:value -> [IP]:count
-// var client redis.Conn
+func RedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	pong, err := client.Ping().Result()
+	log.Printf("Connect Redis Success !! ")
+	log.Printf(pong, err)
 
-func getVisitor(client redis.Conn, IP string) int {
-	exist, err := redis.Bool(client.Do("EXISTS", IP))
-	if err != nil {
-		log.Panic("client EXISTS error: ", err)
-	}
-	log.Println("exist ", exist)
-	log.Println("IP ", IP)
-
-	if !exist {
-		log.Println("IP is not exist!!!!!!!!!")
-		_, err = client.Do("SET", IP, 1, "EX", "3600") // after 3600s key will expire
-		if err != nil {
-			log.Panic("redis SET failed:", err)
-		}
-		return 1
-	}
-	var val, _ = client.Do("GET", IP)
-	log.Println("val_type: ", reflect.TypeOf(val))
-	log.Println("val: ", val)
-	// log.Println("val: ", strconv.Atoi(val.(string)))
-	var new_val = val. + 1
-	client.Do("GETSET", IP, new_val)
-
-	log.Println(new_val)
-	return int(new_val)
+	return client
 }
 
 func middleware(c *gin.Context) {
-	client, err := redis.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		log.Panic("Connect to redis error", err)
-	}
-	log.Println("Connect Redis Success !!!!!!")
-	// defer client.Close()
+	client := RedisClient()
 
 	IP := c.ClientIP()
-	var request_count = getVisitor(client, IP)
-	log.Println("count : ", request_count)
+	var ip_counter int
+	var now_count int
 
-	if request_count > count_limiter {
-		var remain_time = strconv.Itoa(request_count - count_limiter)
-		// reply, err := client.Do("TLL", IP)
-		// if err != nil {
-		// 	log.Panic("get IP 'TLL' error: ", err)
-		// } else {
-		// 	log.Println(reply)
-		// 	// var reset_time = strconv.Itoa(reply)
-		// }
+	ip_counter, err := client.Get(IP).Int()
+	if err == redis.Nil {
+		log.Println("IP is not exist!!!!!!!!!")
+		now_count = 1
+		client.Set(IP, now_count, 10*time.Second)
+	}
+
+	ttl, _ := client.TTL(IP).Result()
+	now_count = ip_counter + 1
+	client.Set(IP, now_count, ttl)
+
+	if ttl < time.Duration(0) {
+		client.Del(IP)
+	}
+
+	log.Println("ip_counter: ", now_count)
+
+	if now_count > COUNT_LIMITER {
+		remain_time := strconv.Itoa(now_count - COUNT_LIMITER)
 		c.Writer.Header().Set("X-RateLimit-Remaining", remain_time)
-		c.Writer.Header().Set("X-RateLimit-Reset", remain_time)
-		// log.Println("X-RateLimit-Remaining: ", c.Request.Header["X-RateLimit-Remaining"])
-		// log.Println("X-RateLimit-Reset: ", c.Request.Header["X-RateLimit-Reset"])
+		c.Writer.Header().Set("X-RateLimit-Reset", ttl.String())
 
 		c.String(http.StatusTooManyRequests, "http.StatusTooManyRequests")
 		log.Panic("http.Status(429)")
